266e9556077f53c1415a81384fe3fefdea05beb2
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-02-23 07:46:32 +0600

    update main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+a = 12 # int
+b = 12.2 # float 
+c = "Hi" # string 
+d = [1,3,2,5] # list (array )
+e = {"x": 1,"y": 3} # dict(object )
+f = ("x", "y", "z") # tuple 
+g = 2j # complex 
+h = range(4) # range 
+i = bytes(b"100") # bytes
+j = bytearray(b"10") # bytearray
+
+
+print(a)
+print(type(a), "\n")
+
+print(b)
+print(type(b), "\n")
+
+print(c)
+print(type(c), "\n")
+
+print(d)
+print(type(d), "\n")
+
+print(e)
+print(type(e), "\n")
+
+print(f)
+print(type(f), "\n")
+
+print(g)
+print(type(g), "\n")
+
+print(h)
+print(type(h), "\n")
+
+print(i)
+print(type(i), "\n")
+
+print(j)
+print(type(j), "\n")
+
+
+arr = [1,2,3]
+arr[2] = 100
+
+print(len(arr))
