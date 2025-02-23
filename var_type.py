62f8cde02272aa75270be844ec139997fdba5a52
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-02-23 08:01:21 +0600

    update

---
+++ var_type.py
@@ -0,0 +1,45 @@
+a = 12 # int
+b = 12.2 # float 
+c = "Hi" # string 
+d = [1,3,2,5] # list (array )
+e = {"x": 1,"y": 3} # dict(object )
+f = ("x", "y", "z") # tuple 
+g = 2j # complex 
+h = range(4) # range 
+i = bytes(b"100") # bytes
+j = bytearray(b"10") # bytearray
+k = {1,2,3,4} # set 
+l = frozenset({1,2,3,4}) # frozenset
+m = True # boolean 
+n = memoryview(b"abcd") # memory view 
+o = None # None
+print(a,"type:", type(a), "\n")
+
+print(b,"type:", type(b), "\n")
+
+print(c,"type:", type(c), "\n")
+
+print(d,"type:", type(d), "\n")
+
+print(e,"type:", type(e), "\n")
+
+print(f,"type:", type(f), "\n")
+
+print(g,"type:", type(g), "\n")
+
+print(h,"type:", type(h), "\n")
+
+print(i,"type:", type(i), "\n")
+
+print(j,"type:", type(j), "\n")
+
+print(k,"type:", type(k), "\n")
+
+print(l,"type:", type(l), "\n")
+
+print(m,"type:", type(m), "\n")
+
+print(n,"type:", type(n), "\n")
+
+print(o,"type:", type(o), "\n")
+		
