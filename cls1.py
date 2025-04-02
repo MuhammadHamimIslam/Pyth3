a19da048207d2c0a174349710a202d6beabc533e
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-04-02 22:21:43 +0600

    update

--- cls1.py
+++ cls1.py
@@ -78,7 +78,7 @@ class student:
     def getName(self):
         return self.__name
     # function for getting age
-    def getAgr(self):
+    def getAge(self):
         return self.__age
     # function for setting name 
     def setName(self, name):
@@ -86,14 +86,15 @@ class student:
     # function for setting age
     def setAge(self, age):
         self.__age = age
-        
-    
+    # now set property 
+    name = property(getName, setAge, "name")
+    age = property(getAge, setAge, "age")
+
+student1 = student("xyz", 15)
+print(f"student's name is: {student1.name}")
 
 "Some functions for classes"
-
 "-> hasattr(obj, name) to check if an attribute is present"
-
-
 
 print(hasattr(iPhone, "model")) # returns true if "mode" attribute is present 
 
@@ -143,52 +144,3 @@ print(phone.__doc__) # returns tje docstrings inside the class
 print(phone.__name__) # returns the class's name
 print(phone.__module__) # returns the module where the class is defined 
 print(phone.__bases__) # returns the base class
-
-"magic method of class"
-"Magic methods are automatically called whenever the class is used"
-
-class shape:
-    # data of object 
-    def __init__(self, name: str, length: int, width: int, height = 0):
-        # adding data to object 
-        self.name = name
-        self.length = length
-        self.width = width
-        self.height = height
-        
-        # data validation 
-        assert length >= 0, f"length can't be less than or equal to zero!"
-        assert width >= 0, f"width can't be less than or equal to zero!"
-    # custom string representation 
-    def __str__(self): 
-        return f"It's a {self.name}. It's length is {self.length}, width is {self.width} and height is {self.height}"
-    # addition 
-    def __add__(self, other):
-        return self.length + other.length
-    # less than 
-    def __lt__(self, other):
-        return self.width < other.width
-    # greater than 
-    def __gt__(self, other):
-        return self.length > other.length 
-# all objects     
-shape1 = shape("square", 100, 100)
-shape2 = shape("rectangle", 120, 80)
-shape3 = shape("cube", 90, 90, 90)
-
-"-> __str__ method"
-# if we print shape1, we'll see the memory addresses of the object not the information. By using __str__ method we can define what we'll see
-print(shape1)
-
-"-> __add__ method"
-# if we try to add any value of a object, we'll get an error. By using __add__ method we can define it
-print(f"The sum of lengths is: {shape1 + shape2}")
-
-"-> __lt__ method"
-# if we try to use ">" to compare 2 objects, we'll get an error. By using __lt__ method we can define it
-print(shape1 < shape2)
-
-"-> using __gt__ method"
-# if we try to use "<" to compare 2 objects, we'll get an error. By using __gt__ method we can define it
-print(shape1 > shape2)
-
--- cls2.py
+++ cls2.py
@@ -34,3 +34,70 @@ Then need to change the discount from instance level then apply different discou
 # If we want to apply 30% discount for Meats then we'll change it from instance level 
 item2.discount = 0.3
 print(f"Total price of meat after 30% discount: {item2.totalPrice()}")
+
+"magic method of class"
+"Magic methods are automatically called whenever the class is used"
+
+class shape:
+    # data of object 
+    def __init__(self, name: str, length: int, width: int, height = 0):
+        # adding data to object 
+        self.name = name
+        self.length = length
+        self.width = width
+        self.height = height
+        
+        # data validation 
+        assert length >= 0, f"length can't be less than or equal to zero!"
+        assert width >= 0, f"width can't be less than or equal to zero!"
+    # custom string representation 
+    def __str__(self): 
+        return f"It's a {self.name}. It's length is {self.length}, width is {self.width} and height is {self.height}"
+    # addition 
+    def __add__(self, other):
+        return self.length + other.length
+    # less than 
+    def __lt__(self, other):
+        return self.width < other.width
+    # greater than 
+    def __gt__(self, other):
+        return self.length > other.length 
+# all objects     
+shape1 = shape("square", 100, 100)
+shape2 = shape("rectangle", 120, 80)
+shape3 = shape("cube", 90, 90, 90)
+
+"-> __str__ method"
+# if we print shape1, we'll see the memory addresses of the object not the information. By using __str__ method we can define what we'll see
+print(shape1)
+
+"-> __add__ method"
+# if we try to add any value of a object, we'll get an error. By using __add__ method we can define it
+print(f"The sum of lengths is: {shape1 + shape2}")
+
+"-> __lt__ method"
+# if we try to use ">" to compare 2 objects, we'll get an error. By using __lt__ method we can define it
+print(shape1 < shape2)
+
+"-> using __gt__ method"
+# if we try to use "<" to compare 2 objects, we'll get an error. By using __gt__ method we can define it
+print(shape1 > shape2)
+
+"class inheritance"
+
+"Single inheritance"
+# parent class
+class parent: 
+    def parentMethod(self): # a parent method 
+        print("This is a parent method")
+
+# child class
+class child(parent): 
+    def childMethod(self): 
+        print("This is a child method")
+
+# a child object 
+child1 = child()
+# by using child object, we can access both parent and child method, variables, function
+child1.childMethod()
+child1.parentMethod()
