7eb429d817247128e78762ff1eb3931323823be5
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-04-10 23:10:51 +0600

    update cls3.py

--- cls3.py
+++ cls3.py
@@ -37,3 +37,41 @@ sqr = square()
 # draw shape now
 drawShape(rect)
 drawShape(sqr)
+
+"Abstraction"
+# Abstraction is a process in which only the relevant data about an object is exposed, and hided all the other details
+class food:
+    def __wash(self): # wash all ingredients 
+        print("Ingredients are being washed")
+    def __addIngredients(self): # add all ingredients 
+        print("All ingredients are added")
+    def __cook(self): #finish cooking 
+        print("Cooking is finished")
+    def prepareFood(self): # final function that calls all sub function to finish the food preparation
+        self.__wash()
+        self.__addIngredients()
+        self.__cook()
+    # As the sun functions are private, We can't call it from outside of the class
+
+Rice = food()
+Rice.prepareFood()
+
+"Inner class"
+# inner class mean class inside a class
+class Bangladesh: 
+    def __init__(self, area, population):
+        self.area = area
+        self.population = population
+        self.divisionName = self.divisions().names # get the division's name using slef.InnerClass().property 
+        self.division = self.divisions().division # get all division num
+    # representative of objects 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.area}', '{self.population}', {self.divisionName}, {self.division})"
+    # inner class
+    class divisions: 
+        def __init__(self):
+            self.names = sorted(["Dhaka", "Barishal", "Khulna", "Chittagong", "Rajshahi", "Mymensingh", "Rangpur", "Sylhet"])
+            self.division = 8
+
+bd = Bangladesh("147570km", "168M")
+print(bd)
