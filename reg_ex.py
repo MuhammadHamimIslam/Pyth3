51ba6527f9521eeb7a245ad893d0435d0b874d3c
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-04-12 22:47:45 +0600

    update reg_ex.py

--- reg_ex.py
+++ reg_ex.py
@@ -2,10 +2,15 @@ import re
 
 # A regular expression is a sequence of character to perform search pattern 
 
+"""re class has several function:
+    re.search()
+    re.findall()
+    """
+
 text = "Bangladesh is the most beautiful county in the world"
 
 # now search pattern 
 " '^text' -> starts with text"
-x = re.search("^Bangladesh.*world$", text)
+x = re.findall("^Bangladesh.*world$", text)
 
 print(x)
