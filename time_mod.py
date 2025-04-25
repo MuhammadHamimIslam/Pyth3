ab61f31e462530d72d3155f40eb02a35c697b765
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-04-25 11:14:47 +0600

    update time_mod.py

--- time_mod.py
+++ time_mod.py
@@ -2,6 +2,10 @@ import time
 
 ticks = time.time()
 print(ticks) # returns the current system time in  ticks since 12:00am, January 1, 1970(epoch)
+"time.asctime([t])"
+
+t = time.asctime()
+
 
 "format the time"
 
