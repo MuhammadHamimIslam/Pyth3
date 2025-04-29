e8f003db7f0a2b4c0de31de8989b0cc99740dc83
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-04-29 22:08:16 +0600

    update calendar_mod.py

--- calendar_mod.py
+++ calendar_mod.py
@@ -9,3 +9,14 @@ m -> The number of rows per month
 """
 
 print(calendar.calendar(2025)) 
+
+"calendar.isleap(year)"
+
+print(calendar.isleap(2025)) # returns if the year is leap year
+
+"calendar.leapdays(year1, year2)"
+print(2000, 2025) # returns number of leap years between the given years (excluding the end year).
+
+"calendar.month(year, month, w=2, l=1)"
+
+print(calendar.month(2025, 4)) # returns the calendar of the give month 
